14ce2db85903b20388fb00138c2737365bf74f1a
Daniel Sarbe <dsarbe@sdl.com>
2016-05-21 21:12:55 +0300

    Some other examples

--- samples/src/main/java/teamoptimizer/TeamOptimizerSpeechletRequestStreamHandler.java
+++ samples/src/main/java/teamoptimizer/TeamOptimizerSpeechletRequestStreamHandler.java
@@ -7,7 +7,7 @@
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-package historybuff;
+package teamoptimizer;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,11 +18,11 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 /**
  * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
  * experience. To do this, simply set the handler field in the AWS Lambda console to
- * "historybuff.HistoryBuffSpeechletRequestStreamHandler" For this to work, you'll also need to
+ * "teamoptimizer.TeamOptimizerSpeechletRequestStreamHandler" For this to work, you'll also need to
  * build this project using the {@code lambda-compile} Ant task and upload the resulting zip file to
  * power your function.
  */
-public class HistoryBuffSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+public class TeamOptimizerSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
 
     private static final Set<String> supportedApplicationIds;
 
@@ -32,15 +32,15 @@ public class HistoryBuffSpeechletRequestStreamHandler extends SpeechletRequestSt
          * Alexa Skill and put the relevant Application Ids in this Set.
          */
         supportedApplicationIds = new HashSet<String>();
-        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.65c23d71-96d4-4de7-9830-f23e47ad266b");
+        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.588fad21-567f-406b-97cc-a9cef6762283");
     }
 
-    public HistoryBuffSpeechletRequestStreamHandler() {
-        super(new HistoryBuffSpeechlet(), supportedApplicationIds);
+    public TeamOptimizerSpeechletRequestStreamHandler() {
+        super(new TeamOptimizerSpeechlet(), supportedApplicationIds);
     }
 
-    public HistoryBuffSpeechletRequestStreamHandler(Speechlet speechlet,
-            Set<String> supportedApplicationIds) {
+    public TeamOptimizerSpeechletRequestStreamHandler(Speechlet speechlet,
+                                                      Set<String> supportedApplicationIds) {
         super(speechlet, supportedApplicationIds);
     }
 
